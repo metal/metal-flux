872d67f12b4930f1415687e61052d5017d452acd
Maira Bello <maira.araujo@liferay.com>
2015-10-22 10:19:20 -0300

    Implements the FluxStore

--- src/FluxStore.js
+++ src/FluxStore.js
@@ -1,7 +1,43 @@
 'use strict';
 
-class FluxStore {
+import EventEmitter from 'bower:metal/src/events/EventEmitter';
 
+/**
+ * The store base class for use in a Flux architecture
+ * (https://facebook.github.io/flux/).
+ */
+class FluxStore extends EventEmitter {
+	constructor(dispatcher) {
+		super();
+		this.dispatcher_ = dispatcher;
+		this.dispatcherId_ = dispatcher.register(this.handleDispatch_.bind(this));
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	disposeInternal() {
+		super.disposeInternal();
+		this.dispatcher_.unregister(this.dispatcherId_);
+		this.dispatcher_ = null;
+	}
+
+	/**
+	 * Handles a dispatch from this store's dispatcher. Should be overridden
+	 * by subclasses.
+	 * @protected
+	 */
+	handleDispatch_() {
+		throw new Error('FluxStore.handleDispatch_: Needs to be implemented by subclasses.');
+	}
 }
+
+/**
+ * The name of the event that `FluxStore` should emit when a dispatch causes
+ * the store's data to change.
+ * @type {string}
+ * @static
+ */
+FluxStore.EVENT_CHANGE = 'change';
 
 export default FluxStore;
--- test/FluxStore.js
+++ test/FluxStore.js
@@ -1,5 +1,58 @@
 'use strict';
 
-describe('FluxStore', function() {
+import EventEmitter from 'bower:metal/src/events/EventEmitter';
+import FluxDispatcher from '../src/FluxDispatcher';
+import FluxStore from '../src/FluxStore';
 
+describe('FluxStore', function() {
+	var store;
+
+	afterEach(function() {
+		store.dispose();
+	});
+
+	it('should extend from EventEmitter', function() {
+		store = new FluxStore(new FluxDispatcher());
+		assert.ok(store instanceof EventEmitter);
+	});
+
+	it('should call `handleDispatch_` method automatically for subclasses on dispatch', function() {
+		var receivedPayload;
+		class TestStore extends FluxStore {
+			handleDispatch_(payload) {
+				receivedPayload = payload;
+			}
+		}
+
+		var dispatcher = new FluxDispatcher();
+		store = new TestStore(dispatcher);
+		var sentPayload = {};
+		dispatcher.dispatch(sentPayload);
+
+		assert.strictEqual(receivedPayload, sentPayload);
+	});
+
+	it('should throw error if `handleDispatch_` is not implemented by subclass', function() {
+		class TestStore extends FluxStore {
+		}
+		var dispatcher = new FluxDispatcher();
+		store = new TestStore(dispatcher);
+		assert.throws(() => dispatcher.dispatch());
+	});
+
+	it('should not call `handleDispatch_` method automatically for subclasses after dispose', function() {
+		var called;
+		class TestStore extends FluxStore {
+			handleDispatch_() {
+				called = true;
+			}
+		}
+
+		var dispatcher = new FluxDispatcher();
+		store = new TestStore(dispatcher);
+		store.dispose();
+		dispatcher.dispatch({});
+
+		assert.ok(!called);
+	});
 });
