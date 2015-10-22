cfbd06ae59cd3371bd0f2da2bf5d825548795e63
Maira Bello <maira.araujo@liferay.com>
2015-10-22 09:35:03 -0300

    Implements the FluxDispatcher

--- src/FluxDispatcher.js
+++ src/FluxDispatcher.js
@@ -1,7 +1,110 @@
 'use strict';
 
-class FluxDispatcher {
+import Disposable from 'bower:metal/src/disposable/Disposable';
 
+/**
+ * The dispatcher class for use in a Flux architecture
+ * (https://facebook.github.io/flux/).
+ */
+class FluxDispatcher extends Disposable {
+	constructor() {
+		super();
+		this.callbacks_ = {};
+		this.hasStarted_ = {};
+		this.isDone_ = {};
+		this.nextId_ = 1;
+	}
+
+	/**
+	 * Dispatches the given payload to all registered callbacks.
+	 * @param {!Object} payload
+	 */
+	dispatch(payload) {
+		if (this.isDispatching_) {
+			throw new Error('FluxDispatcher.dispatch: Can\'t dispatch during another dispatch');
+		}
+
+		try {
+			this.isDispatching_ = true;
+			this.currentPayload_ = payload;
+			var ids = Object.keys(this.callbacks_);
+			for (var i = 0; i < ids.length; i++) {
+				if (!this.hasStarted_[ids[i]]) {
+					this.runCallback_(ids[i]);
+				}
+			}
+		} finally {
+			this.isDispatching_ = false;
+			this.currentPayload_ = null;
+			this.isDone_ = {};
+			this.hasStarted_ = {};
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	disposeInternal() {
+		this.callbacks_ = null;
+		this.currentPayload_ = null;
+		this.hasStarted_ = null;
+		this.isDone_ = null;
+	}
+
+	/**
+	 * Registers the given callback, so it can receive payloads from dispatches.
+	 * @param {!function(!Object)} callback [description]
+	 * @return {number} The registered callback's id. Can be used to unregister it.
+	 */
+	register(callback) {
+		var id = this.nextId_++;
+		this.callbacks_[id] = callback;
+		return id;
+	}
+
+	/**
+	 * Runs the callback for the given id. Also updates the callback's state info.
+	 * @param {number} id
+	 * @protected
+	 */
+	runCallback_(id) {
+		this.hasStarted_[id] = true;
+		this.callbacks_[id](this.currentPayload_);
+		this.isDone_[id] = true;
+	}
+
+	/**
+	 * Unregisters the callback with the given id.
+	 * @param {number} id The id of a callback registered through `FluxDispatcher.register`.
+	 */
+	unregister(id) {
+		delete this.callbacks_[id];
+	}
+
+	/**
+	 * Waits until the callbacks for the given ids are done being called on this dispatch.
+	 * @param {!Array<number>|number} ids
+	 */
+	waitFor(ids) {
+		if (!this.isDispatching_) {
+			throw new Error('FluxDispatcher.waitFor: Must be called during dispatch.');
+		}
+
+		ids = Array.isArray(ids) ? ids : [ids];
+		for (var i = 0; i < ids.length; i++) {
+			if (this.hasStarted_[ids[i]]) {
+				if (!this.isDone_[ids[i]]) {
+					throw new Error(
+						`FluxDispatcher.waitFor: Circular dependency detected while waiting for ${ids[i]}.`
+					);
+				}
+			} else if (!this.callbacks_[ids[i]]) {
+				throw new Error(`FluxDispatcher.waitFor: No registered callback with id ${ids[i]}.`);
+			} else {
+				this.runCallback_(ids[i]);
+			}
+		}
+	}
 }
 
 export default FluxDispatcher;
--- test/FluxDispatcher.js
+++ test/FluxDispatcher.js
@@ -1,5 +1,154 @@
 'use strict';
 
-describe('FluxDispatcher', function() {
+import FluxDispatcher from '../src/FluxDispatcher';
 
+describe('FluxDispatcher', function() {
+	var dispatcher;
+
+	beforeEach(function() {
+		dispatcher = new FluxDispatcher();
+	});
+
+	afterEach(function() {
+		dispatcher.dispose();
+	});
+
+	it('should run registered callbacks passing the dispatched payload', function() {
+		var callback1 = sinon.stub();
+		var callback2 = sinon.stub();
+
+		dispatcher.register(callback1);
+		dispatcher.register(callback2);
+
+		var payload = {};
+		dispatcher.dispatch(payload);
+
+		assert.strictEqual(1, callback1.callCount);
+		assert.strictEqual(payload, callback1.args[0][0]);
+		assert.strictEqual(1, callback2.callCount);
+		assert.strictEqual(payload, callback2.args[0][0]);
+	});
+
+	it('should not run unregistered callbacks when payload is dispatched', function() {
+		var callback1 = sinon.stub();
+		var callback2 = sinon.stub();
+
+		dispatcher.register(callback1);
+		var id2 = dispatcher.register(callback2);
+		dispatcher.unregister(id2);
+
+		var payload = {};
+		dispatcher.dispatch(payload);
+
+		assert.strictEqual(1, callback1.callCount);
+		assert.strictEqual(payload, callback1.args[0][0]);
+		assert.strictEqual(0, callback2.callCount);
+	});
+
+	it('should throw error if trying to dispatch during another dispatch', function() {
+		dispatcher.register(() => dispatcher.dispatch({}));
+		assert.throws(function() {
+			dispatcher.dispatch({});
+		});
+	});
+
+	it('should be able to dispatch again after a dispatch caused an error', function() {
+		var id = dispatcher.register(() => {
+			throw new Error();
+		});
+		var callback = sinon.stub();
+		dispatcher.register(callback);
+
+		assert.throws(function() {
+			dispatcher.dispatch({});
+		});
+		assert.strictEqual(0, callback.callCount);
+
+		dispatcher.unregister(id);
+		dispatcher.dispatch({});
+		assert.strictEqual(1, callback.callCount);
+	});
+
+	describe('waitFor', function() {
+		it('should throw error if `waitFor` is called outside a dispatch', function() {
+			var id = dispatcher.register(sinon.stub());
+			assert.throws(() => dispatcher.waitFor(id));
+		});
+
+		it('should allow a callback to wait for another callback to finish', function() {
+			var callback1 = sinon.spy(() => {
+				assert.strictEqual(0, callback2.callCount);
+				assert.strictEqual(0, callback3.callCount);
+
+				dispatcher.waitFor(id2);
+				assert.strictEqual(1, callback2.callCount);
+				assert.strictEqual(0, callback3.callCount);
+			});
+
+			var callback2 = sinon.stub();
+			var callback3 = sinon.stub();
+			dispatcher.register(callback1);
+			var id2 = dispatcher.register(callback2);
+			dispatcher.register(callback3);
+
+			dispatcher.dispatch({});
+			assert.strictEqual(1, callback1.callCount);
+		});
+
+		it('should allow a callback to wait for multiple another callbacks to finish', function() {
+			var callback1 = sinon.spy(() => {
+				assert.strictEqual(0, callback2.callCount);
+				assert.strictEqual(0, callback3.callCount);
+
+				dispatcher.waitFor([id2, id3]);
+				assert.strictEqual(1, callback2.callCount);
+				assert.strictEqual(1, callback3.callCount);
+			});
+
+			var callback2 = sinon.stub();
+			var callback3 = sinon.stub();
+			dispatcher.register(callback1);
+			var id2 = dispatcher.register(callback2);
+			var id3 = dispatcher.register(callback3);
+
+			dispatcher.dispatch({});
+			assert.strictEqual(1, callback1.callCount);
+		});
+
+		it('should run callback immediately if it waits for another that has already run', function() {
+			var callback1 = sinon.stub();
+			var callback2 = sinon.spy(() => {
+				assert.strictEqual(1, callback1.callCount);
+				dispatcher.waitFor(id1);
+			});
+			var id1 = dispatcher.register(callback1);
+			dispatcher.register(callback2);
+
+			dispatcher.dispatch({});
+			assert.strictEqual(1, callback2.callCount);
+		});
+
+		it('should throw error if circular dependency is detected while waiting for callbacks', function() {
+			var callback1 = sinon.spy(() => {
+				dispatcher.waitFor(id2);
+			});
+			var callback2 = sinon.spy(() => {
+				assert.throws(() => dispatcher.waitFor(id1));
+			});
+			var id1 = dispatcher.register(callback1);
+			var id2 = dispatcher.register(callback2);
+
+			dispatcher.dispatch({});
+			assert.strictEqual(1, callback1.callCount);
+			assert.strictEqual(1, callback2.callCount);
+		});
+
+		it('should throw error if callback requests waiting for unregistered callback', function() {
+			var callback = sinon.spy(() => {
+				assert.throws(() => dispatcher.waitFor(3));
+			});
+			dispatcher.register(callback);
+			dispatcher.dispatch({});
+		});
+	});
 });
